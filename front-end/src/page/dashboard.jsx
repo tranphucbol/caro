b3a2edd2aa82a2ffab29217d397faf9fd7541548
tranphucbol <tranphucbol@gmail.com>
2019-08-12 16:14:10 +0700

    route animation

---
+++ front-end/src/page/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from './main'
+import { Link } from "react-router-dom";
+
+const DashBoard = () => (
+    <Main>
+        <div className="row min-vh-100">
+            <Link to="/login">login</Link>
+        </div>
+    </Main>
+)
+
+export default DashBoard
